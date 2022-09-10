1ff6aa4da8301fc1b7430180eb22343e1c3f6ffa
iMidnight <mohaidarus@gmail.com>
2022-09-10 15:50:30 +0100

    Moved CMD handler  file and added feed handler

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const colors =      require('colors');
-const npmUpdater =  require('./utils/updater/npm');
+const colors = require('colors');
+const npmUpdater = require('./utils/updater/npm');
 const fileUpdater = require('./utils/updater/files');
-
 const consolePrefix = `${"[".blue}${"dbd-soft-ui".yellow}${"]".blue} `;
 
 module.exports = (themeConfig = {}) => {
-
     return {
         themeCodename: 'softui',
         viewsPath: require('path').join(__dirname, '/views'),
         staticPath: require('path').join(__dirname, '/views/src'),
         themeConfig: {
             ...themeConfig,
-            defaultLocales: require('./locales.js'), 
+            defaultLocales: require('./locales.js'),
         },
         messages: {
             error: {
@@ -30,7 +28,7 @@ module.exports = (themeConfig = {}) => {
                 await fileUpdater.update();
                 if (!check) outdated = true;
             })();
-            
+
             require('./utils/functions/errorHandler')(config, themeConfig)
             require('./utils/functions/settingsPage')(config, themeConfig)
             await require('./utils/addonManager').execute(themeConfig, config, app, module.exports.messages);
@@ -39,46 +37,6 @@ module.exports = (themeConfig = {}) => {
     }
 }
 
-module.exports.cmdHandler = (commands, prefix) => {
-    if (!commands) throw new Error('No commands were provided to the Soft UI cmdHandler.');
-    if (!prefix) prefix = "!";
 
-    let finalCategories = [];
-
-    for (const command of commands) {
-        if (!command.category) continue;
-        if (!categories?.includes(command.category)) categories.push(command.category);
-    }
-
-    for (const category of categories) {
-        if (category === "admin" || category === "owner" || category === "development") continue;
-        let commandsArr = [];
-
-        for (const command of commands) {
-            if (command.category === category) {
-                let obj = {
-                    commandName: command.name,
-                    commandUsage: `${prefix}${command.name} ${command.usage}`,
-                    commandDescription: command.description,
-                    commandAlias: command.aliases?.join(', ') || 'None',
-                }
-                commandsArr.push(obj);
-            }
-        }
-
-        const categoryObj = {
-            categoryId: category,
-            category: `${capitalizeFirstLetter(category)}`,
-            subTitle: `${capitalizeFirstLetter(category)} commands`,
-            list: commandsArr,
-        }
-
-        finalCategories.push(categoryObj);
-    }
-
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-    return finalCategories
-}
+module.exports.Feed = require('./utils/feedHandler');
+module.exports.cmdHandler = require('./utils/cmdHandler');
