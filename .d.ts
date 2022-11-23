4f5e72bbbc32ce3826c0dc8022ffa74a02e8c015
Marcin Kondrat <breftejk@gmail.com>
2022-11-23 16:15:40 +0100

    typings (.d.ts)

---
+++ .d.ts
@@ -0,0 +1,7 @@
+declare module "dbd-soft-ui" {
+    export default function (options: any): any;
+    const partials: any;
+    const formTypes: any;
+    const Feed: any;
+    const cmdHandler: any;
+}
