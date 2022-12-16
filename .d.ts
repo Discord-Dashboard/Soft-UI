a8c9c38c1a95b37cd2eabe54356b7fe251c4bc62
Mechite <mechitebusiness@gmail.com>
2022-12-16 01:53:37 +0000

    Improve type declarations

--- .d.ts
+++
@@ -1,7 +0,0 @@
-declare module "dbd-soft-ui" {
-    export default function (options: any): any;
-    const partials: any;
-    const formTypes: any;
-    const Feed: any;
-    const cmdHandler: any;
-}
